dcb7d67dd859ef2f1bc3dd430e8157961bd15484
Leah Isenor <leah.isenor@dal.ca>
2022-03-01 20:08:23 -0400

    removed unused imports

--- app/src/main/java/com/example/readingassistant/persistence/Database.kt
+++ app/src/main/java/com/example/readingassistant/persistence/Database.kt
@@ -1,12 +1,10 @@
 package com.example.readingassistant.persistence
 
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
 class Database {
 
-    public fun getConnection(): FirebaseFirestore {
+    fun getConnection(): FirebaseFirestore {
         return FirebaseFirestore.getInstance();
     }
 
