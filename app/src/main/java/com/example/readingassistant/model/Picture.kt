0704ebd8b623ed7778539bc4d7a0f0e74d5833d8
Manraj Singh <mn697903@dal.ca>
2022-03-27 18:10:08 -0300

    Revert "gallary done"
    This reverts commit 05670704655e6a3cfed95862fb0098ec5603d85e

--- app/src/main/java/com/example/readingassistant/model/Picture.kt
+++
@@ -1,9 +0,0 @@
-package com.example.readingassistant.model
-
-import com.google.firebase.database.IgnoreExtraProperties
-import java.util.*
-
-@IgnoreExtraProperties
-data class Picture(var name: String = "",
-                   var date: String = "",
-                   var url: String = "")
